9fe1fd290f75d2af2292e273e4cb3a72b303250c
Antonio Barrera <Barrera.antonio@gmail.com>
2021-04-29 18:41:29 -0700

    updated models

---
+++ models/Plant.js
@@ -0,0 +1,52 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Plant extends Model {}
+
+Plant.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement:true,
+        },
+        name:{
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        plant_habit:{
+            type: DataTypes.STRING,
+        },
+        life_cycle:{
+            type: DataTypes.STRING,
+        },
+        sun_req:{
+            type: DataTypes.STRING,
+        },
+        water_pref:{
+            type: DataTypes.STRING,
+        },
+        uses:{
+            type: DataTypes.STRING,
+        },
+        edible_parts:{
+            type: DataTypes.STRING,
+        },
+        toxic_parts:{
+            type: DataTypes.STRING,
+        },
+        time_to_fruit:{
+            type: DataTypes.INTEGER,
+        },
+    }
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'plant',
+    }
+);
+
+module.exports = Plant;
--- models/index.js
+++ models/index.js
@@ -1,3 +1,21 @@
 const User = require('./User');
+<<<<<<< HEAD
+const Garden = require('./Garden');
+const Plant = require('./Plant');
+
+User.hasMany(Garden,{
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
+
+Garden.belongsTo(User, {
+    foreignKey:'user_id'
+});
+
+Garden.hasMany(Plant,{
+    foreignKey: 'name'
+});
+=======
 
 module.exports = { User };
+>>>>>>> 9b61799a34f9b60a737804a8dbaa8ec521d79b79
