d5d523cb329780f04bfe9e7da72aefbb2ebd7deb
Rayna W <raynagweinreb@gmail.com>
2021-05-01 11:35:01 -0700

    JS files

--- models/index.js
+++ models/index.js
@@ -15,13 +15,9 @@ Garden.hasMany(Plant,{
     foreignKey: 'id'
 });
 
-<<<<<<< HEAD
-module.exports = { User, Garden };
-=======
 Plant.belongsToMany(Garden,{
     foreignKey:'plant_id'
 })
 
 module.exports = { User, Garden, Plant };
 
->>>>>>> 0498a457dd52f2d84f4e7409ee81479bb83222f5
