d6fb9a802b9fd0819cdf49fad4a8b98f5640903b
Antonio Barrera <Barrera.antonio@gmail.com>
2021-05-05 17:44:23 -0700

    homepage route edits

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const sequelize = require('../config/connection');
 const { User, Plant, Garden, PlantTag } = require('../models');
 const withAuth = require('../utils/auth');
 
@@ -6,47 +7,53 @@ const withAuth = require('../utils/auth');
 //The homepage is the login page, if the user is already logged in, they will be redirected to their userpage
 router.get('/', (req, res) => {
   // If a session exists, redirect the request to the homepage
-  if (req.session.loggedIn) {
-    res.redirect('/userpage');
-    return;
-  }
+  // if (req.session.loggedIn) {
+  //   res.redirect('/userpage');
+  //   return; }
 
   res.render('homepage');
 });
 
 // use withAuth middleware to prevent access
-router.get('/userpage', withAuth, async (req, res) => {
+router.get('/userpage/:id', async (req, res) => {
 
   try {
     // Get all users, sorted by name
-    const userData = await User.findAll({
-      attributes: ['name'],
-      include: [{
-        model: Garden,
-        attributes: ['id', 'garden_name', 'user_id', 'plant_id'],
-        include: {
+    const userData = await User.findOne({
+      where:{
+        id: req.params.id
+      },
+      attributes: ['id', 'name'] ,
+      include: [
+        {
+          model: Garden,
+          attributes: ['id', 'garden_name', 'user_id', 'plant_id'],
+        },
+        {
           model: PlantTag,
           attributes: ['id', 'plant_id', 'garden_id'],
         },
-        include: {
+      {
           model: Plant,
           attributes: ['id', 'name', 'time_to_fruit']
         }
-      }]
+      ]
     });
+    if (!userData){
+      res.status(404).json({ message: 'No user found with this id' });
+    }
+// Serialize user data so templates can read it
+const users = userData.map((users) => users.get({ plain: true }));
 
-    // Serialize user data so templates can read it
-    const users = userData.map((project) => project.get({ plain: true }));
-
-    // Pass serialized data into Handlebars.js template
-    res.render('userpage', {
-      users,
-      // Pass the logged in flag to the template
-      loggedIn: req.session.loggedIn
-    });
+// Pass serialized data into Handlebars.js template
+res.render('userpage', {
+  users,
+  // Pass the logged in flag to the template
+  loggedIn: req.session.loggedIn
+});
   } catch (err) {
-    res.status(500).json(err);
-  }
+  res.status(500).json(err);
+}
 });
 
 //plant information page by id
@@ -60,8 +67,8 @@ router.get('/plantpage/:id', withAuth, async (req, res) => {
   }
 });
 //We should only need plant data on this page
-router.get('/creategarden', withAuth, async (req, res) =>{
-  try{
+router.get('/creategarden', withAuth, async (req, res) => {
+  try {
     const plantData = await Plant.findAll();
 
     const plant = plantData.get({ plain: true });
